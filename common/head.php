4275d5280b5ef7fc1c9fb502b38eec607b6bb3f7
adnene_hamzaoui <adnene.hamzaoui@gmail.com>
2023-02-08 20:44:08 +0100

    added favicon

--- common/head.php
+++ common/head.php
@@ -4,4 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio - Adnène</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favicon/favicon-16x16.png">
 </head>
