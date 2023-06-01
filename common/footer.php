25320b3374089cf81e86d9253a0241b8b4ef4a75
adnene_hamzaoui <adnene.hamzaoui@gmail.com>
2023-06-01 20:50:14 +0200

    changes.txt

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,4 @@
 <footer>
-<span class="copyright-text"><?=$f_copyrights?></span>
+    <span class="copyright-text"><?= $f_copyrights ?></span>
+    <a href="https://metzys.net" class="logo-metzys"></a>
 </footer>
